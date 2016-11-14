d2e2edd4983cdfc0380e49f8f365530939b38db4
ShungoIto <g2116002@fun.ac.jp>
2016-11-14 09:38:38 +0900

    home画面作成

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,11 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+function searchFeeling(){
+    var feeling = document.searchForm.feelings.value;
+    console.log(feeling);
+    window.location.href = 'search.html';
+}
+
+function iconSelect(){
+ 
+}
