fcdd68bc9656e055fcdd269dd16e50d2a8e6641b
takhos1411 <m5201204@u-aizu.ac.jp>
2016-11-29 17:15:23 +0900

    db,table,seeds seting done!

--- Gemfile
+++ Gemfile
@@ -21,6 +21,11 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# Use jquery
+gem 'jquery-rails'
+# Use Google map
+#gem 'gmaps4rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118022253) do
+ActiveRecord::Schema.define(version: 20161129055723) do
 
   create_table "feelings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "memid"
     t.integer  "spotid"
-    t.string   "emotion"
+    t.integer  "emotion"
     t.text     "comment",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
