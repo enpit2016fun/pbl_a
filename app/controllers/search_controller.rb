871a52ba4b3a002531594dcfdaae30279a4e078a
ShungoIto <g2116002@fun.ac.jp>
2016-12-08 14:25:58 +0900

    位置情報が重複しないようにDBのデータを取得

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,44 +3,42 @@ class SearchController < ApplicationController
   end
 
   def anticipacion
-    @anticipacion = Sensation.where(emotion: 1)
-    gon.sensation = Sensation.where(emotion: 1)
+    @anticipacion = Sensation.select("lon, lat, emotion").uniq.where(emotion: 1)
+    gon.sensation = Sensation.select("lon, lat, emotion").uniq.where(emotion: 1)
   end
 
   def joy
-    @joy = Sensation.where(emotion: 2)
-    gon.sensation = Sensation.where(emotion: 2)
+    @joy = Sensation.select("lon, lat, emotion").uniq.where(emotion: 2)
+    gon.sensation = Sensation.select("lon, lat, emotion").uniq.where(emotion: 2)
   end
 
   def trust
-    @trust = Sensation.where(emotion: 3)
-    #gon.trust = Sensation.where(emotion: 3)
-    gon.sensation = Sensation.where(emotion: 3)
+    @trust = Sensation.select("lon, lat, emotion").uniq.where(emotion: 3)
+    gon.sensation = Sensation.select("lon, lat, emotion").uniq.where(emotion: 3)
   end
   
   def fear
-    @fear = Sensation.where(emotion: 4)
-    gon.sensation = Sensation.where(emotion: 4)
+    @fear = Sensation.select("lon, lat, emotion").uniq.where(emotion: 4)
+    gon.sensation = Sensation.select("lon, lat, emotion").uniq.where(emotion: 4)
   end
 
   def surprise
-    @surprise = Sensation.where(emotion: 5)
-    gon.sensation = Sensation.where(emotion: 5)
+    @surprise = Sensation.select("lon, lat, emotion").uniq.where(emotion: 5)
+    gon.sensation = Sensation.select("lon, lat, emotion").uniq.where(emotion: 5)
   end
 
   def sadness
-    @sadness = Sensation.where(emotion: 6)
-    gon.sensation = Sensation.where(emotion: 6)
+    @sadness = Sensation.select("lon, lat, emotion").uniq.where(emotion: 6)
+    gon.sensation = Sensation.select("lon, lat, emotion").uniq.where(emotion: 6)
   end
 
   def disgust
-    @disgust = Sensation.where(emotion: 7)
-    gon.sensation = Sensation.where(emotion: 7)
+    @disgust = Sensation.select("lon, lat, emotion").uniq.where(emotion: 7)
+    gon.sensation = Sensation.select("lon, lat, emotion").uniq.where(emotion: 7)
   end
 
   def anger
-    @anger = Sensation.where(emotion: 8)
-    gon.sensation= Sensation.where(emotion: 8)
+    @anger = Sensation.select("lon, lat, emotion").uniq.where(emotion: 8)
+    gon.sensation = Sensation.select("lon, lat, emotion").uniq.where(emotion: 8)
   end
-
 end
